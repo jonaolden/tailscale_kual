2752d0547275815670b5146c72c4f6d623ac84e8
jonaolden <oldenburg.jonatan@gmail.com>
2024-11-13 09:44:00 +0100

    Update receive_taildrop.sh
    Added configurable target dir

--- tailscale/bin/receive_taildrop.sh
+++ tailscale/bin/receive_taildrop.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Define directory for Taildrop files
-TAILDROP_DIR="/mnt/us/extensions/tailscale/Taildrop"
+# Read the Taildrop directory path from the file
+TAILDROP_DIR=$(cat /mnt/us/extensions/tailscale/config/tailscale_dir.txt)
 
 # Receive files via Taildrop
 kh_msg "Receiving Taildrop files..."
-/mnt/us/extensions/tailscale/bin/tailscale file get .
+/mnt/us/extensions/tailscale/bin/tailscale file get "$TAILDROP_DIR"
 
 if [ $? -eq 0 ]; then
-    kh_msg "Taildrop files received successfully. Check the Taildrop folder."
+    kh_msg "Taildrop files received successfully. Check the Taildrop folder at $TAILDROP_DIR."
 else
     kh_msg "Failed to receive Taildrop files. Please check the connection."
 fi
